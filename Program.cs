9540ca7939815dbe7267d9fa42004e49cdb67c99
natalie <natalie-hasselmann@web.de>
2021-08-27 09:24:52 +0200

    test

--- Program.cs
+++ Program.cs
@@ -11,7 +11,10 @@ namespace ChatApp
         {
             Console.WriteLine("Hello World!");
             //Participant client = new Participant();
-            
+            Console.WriteLine(Dns.GetHostName());
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            Console.WriteLine(ipAddr);
             Console.ReadKey(true);
         }
     }
